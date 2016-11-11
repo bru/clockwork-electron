ce34e1fc5720e5e57bd0506322535834b0403616
Riccardo Cambiassi <riccardo@chaoscafe.co.uk>
2016-11-11 00:50:35 +0000

    add devtools

--- src_front_profile/clockwork_electron_front/dev/init.cljs
+++ src_front_profile/clockwork_electron_front/dev/init.cljs
@@ -1,9 +1,12 @@
 (ns clockwork-electron-front.init
   (:require [figwheel.client :as fw :include-macros true]
             [clockwork-electron-front.core :as core]
-            [clockwork-electron-front.conf :as conf]))
+            [clockwork-electron-front.conf :as conf]
+            [devtools.core :as devtools]
+            ))
 
 (enable-console-print!)
+(devtools/install!)
 
 (fw/watch-and-reload
  :websocket-url   "ws://localhost:3449/figwheel-ws"
