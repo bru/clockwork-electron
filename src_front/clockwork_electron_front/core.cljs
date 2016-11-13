8b2ae6324fea23d51815ad1b278f5ef995e5eb1e
Riccardo Cambiassi <riccardo@chaoscafe.co.uk>
2016-11-13 16:16:33 +0000

    re-shape core.cljs

--- src_front/clockwork_electron_front/core.cljs
+++ src_front/clockwork_electron_front/core.cljs
@@ -1,8 +1,14 @@
 (ns clockwork-electron-front.core
-  (:require  [reagent.core :as r]
+  (:require  [reagent.core :as reagent]
+             [re-frame.core :refer [dispatch dispatch-sync]]
+             [clockwork-electron-front.db]
+             [clockwork-electron-front.events]
+             ; [clockwork-electron-front.subs]
+             ; [clockwork-electron-front.events]
              [clockwork-electron-front.navigation :as navigation]
+             [clockwork-electron-front.timeslips :as timeslips]
              [clockwork-electron-front.views :as views]
-             [clockwork-electron-front.timeslips :as timeslips]))
+             ))
 
 (defn root-component []
   [:div {:class "page"}
@@ -10,8 +16,9 @@
    [views/today]])
 
 (defn mount-root [setting]
-  (r/render [root-component]
+  (reagent/render [root-component]
             (.getElementById js/document "app")))
 
 (defn init! [setting]
+  (dispatch-sync [:initialise-db])
   (mount-root setting))
