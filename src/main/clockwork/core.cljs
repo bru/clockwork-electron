1d0ba97d619a3797c0adaa5646a59525ec297650
Riccardo Cambiassi <riccardo@chaoscafe.co.uk>
2017-03-23 23:02:28 +0000

    allegedly set icon
    in reality it doesn't work on macos

--- src/main/clockwork/core.cljs
+++ src/main/clockwork/core.cljs
@@ -5,21 +5,29 @@
 (def Electron (nodejs/require "electron"))
 (def app (.-app Electron))
 (def BrowserWindow (.-BrowserWindow Electron))
+(def Tray (.-Tray Electron))
 #_ (def crash-reporter (.-crashReporter Electron))
 
 (def *win* (atom nil))
+(def *tray* (atom nil))
 (def darwin? (= (.-platform nodejs/process) "darwin"))
 
+(def icon (str (.resolve path (js* "__dirname") "../clkwrk.png")))
 (defn create-window []
   (let [win-options (clj->js {:width 800 :height 600
-                              :minWidth 400 :minHeight 512})]
+                              :minWidth 400 :minHeight 512
+                              :icon icon})]
   (reset! *win* (BrowserWindow. win-options))
   ;; when no optimize comment out
   (.loadURL @*win* (str "file://" (.resolve path (js* "__dirname") "../index.html")))
   ;; when no optimize uncomment
   ;; (.loadURL @*win* (str "file://" (.resolve path (js* "__dirname") "../../../index.html")))
-  (.openDevTools (.-webContents @*win*))
+  #_ (.openDevTools (.-webContents @*win*))
   (.on app "closed" (fn [] (reset! *win* nil)))))
+
+(defn set-tray []
+  (reset! *tray* (Tray. icon))
+  (.setImage @*tray* icon))
 
 (defn -main []
   ;; TODO setup crash reports server
@@ -38,7 +46,9 @@
        (fn [] (when darwin? (create-window))))
 
   ;; ready listener
-  (.on app "ready" (fn [] (create-window))))
+  (.on app "ready"(fn []
+                    (create-window)
+                    (set-tray))))
 
 (nodejs/enable-util-print!)
 (.log js/console "Clockwork has started!")
